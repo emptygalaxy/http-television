f5400d3aff76f4c4f407197d2fbfea8c0bb7f5e0
Nick Timman <info@emptygalaxy.com>
2021-02-07 20:17:01 +0100

    Initial commit

---
+++ src/config/HttpTelevisionConfig.ts
@@ -0,0 +1,53 @@
+export interface HttpAction {
+    url?: string;
+    method?: string;
+    headers?: {[key: string]: string};
+    body?: string;
+}
+
+export interface HttpInputAction extends HttpAction {
+    label: string;
+}
+
+export interface HttpTelevisionActions {
+    powerToggle?: HttpAction;
+    powerOn?: HttpAction;
+    powerOff?: HttpAction;
+
+    volumeUp?: HttpAction;
+    volumeDown?: HttpAction;
+
+    up?: HttpAction;
+    down?: HttpAction;
+    left?: HttpAction;
+    right?: HttpAction;
+
+    play?: HttpAction;
+    pause?: HttpAction;
+    playPause?: HttpAction;
+
+    rewind?: HttpAction;
+    forward?: HttpAction;
+
+    select?: HttpAction;
+    back?: HttpAction;
+    info?: HttpAction;
+    exit?: HttpAction;
+
+    inputs?: HttpInputAction[];
+}
+
+export interface HttpTelevisionConfig {
+    name: string;
+    host?: string;
+
+    autoStandby?: number;
+
+    volumeStep?: number;
+    maxVolume?: number;
+
+    global?: HttpAction;
+    actions: HttpTelevisionActions;
+}
+
+export type RemoteKey = 'volumeUp'|'volumeDown'|'left'|'right'|'up'|'down'|'playPause'|'select'|'back'|'info'|'rewind'|'forward';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import {HttpAction, HttpInputAction, HttpTelevisionActions, HttpTelevisionConfig, RemoteKey} from "./config/HttpTelevisionConfig";
+import {HttpTelevision} from "./HttpTelevision";
+
+export {HttpTelevision};
+export {HttpTelevisionConfig, HttpTelevisionActions, HttpAction, HttpInputAction, RemoteKey}
